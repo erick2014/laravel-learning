94ec8d9583c1136957d59225622dcedc3fedad87
Erick Garcia <erickarmandogarcia@hotmail.com>
2016-01-02 09:22:05 -0500

    Add call to withTimestamps method.

--- app/Tag.php
+++ app/Tag.php
@@ -10,6 +10,6 @@ class Tag extends Model
     protected $fillable=["name"];
 
     public function articles(){
-    	return $this->belongsToMany('App\Article');
+    	return $this->belongsToMany('App\Article')->withTimestamps();
     }
 }
